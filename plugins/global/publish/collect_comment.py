79734dc12b3b3ee30fb7fc59854b7b97e0534bee
davidlatwe <david962041@gmail.com>
2018-09-18 20:26:01 +0800

    Fix comment collect plugin

--- plugins/global/publish/collect_comment.py
+++ plugins/global/publish/collect_comment.py
@@ -1,31 +1,13 @@
 import pyblish.api
 
-_COMMENT = ""
-
 
 class CollectComment(pyblish.api.ContextPlugin):
     """This plug-ins displays the comment dialog box per default
-
-    ```
-    context.data {
-            comment: user comment,
-    }
-    ```
-
     """
 
     label = "Publish Comment"
     order = pyblish.api.CollectorOrder - 0.5
 
     def process(self, context):
-        global _COMMENT
         if not context.data.get("comment"):
-            context.data["comment"] = _COMMENT
-
-
-def fetch_comment(comment):
-    global _COMMENT
-    _COMMENT = comment
-
-
-pyblish.api.register_callback("commented", fetch_comment)
+            context.data["comment"] = ""
