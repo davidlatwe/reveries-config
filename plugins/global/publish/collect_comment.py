63ade31ff548c9416f6967c3037b792068b7d442
davidlatwe <david962041@gmail.com>
2018-09-19 20:41:46 +0800

    Update comment collector plugin

--- plugins/global/publish/collect_comment.py
+++ plugins/global/publish/collect_comment.py
@@ -2,12 +2,17 @@ import pyblish.api
 
 
 class CollectComment(pyblish.api.ContextPlugin):
-    """This plug-ins displays the comment dialog box per default
+    """Receive comment from Pyblish GUI comment box
+
+    This initialize "comment" field in context for GUI input.
+
+    If using Pyblish-Lite, this plug-ins will trigger the GUI
+    comment dialog box per default.
+
     """
 
-    label = "Publish Comment"
-    order = pyblish.api.CollectorOrder - 0.5
+    label = "Receive Publish Comment"
+    order = pyblish.api.CollectorOrder - 0.49
 
     def process(self, context):
-        if not context.data.get("comment"):
-            context.data["comment"] = ""
+        context.data["comment"] = ""
