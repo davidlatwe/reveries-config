08c48c78626a9d9218f12ad7e725f77db5b1b5f6
davidlatwe <david962041@gmail.com>
2018-10-03 16:33:28 +0800

    Adding tolerancable matrix matcher

--- reveries/lib.py
+++ reveries/lib.py
@@ -1,4 +1,22 @@
 
+def matrix_equals(a, b, tolerance=1e-10):
+    """
+    Compares two matrices with an imperfection tolerance
+
+    Args:
+        a (list, tuple): the matrix to check
+        b (list, tuple): the matrix to check against
+        tolerance (float): the precision of the differences
+
+    Returns:
+        bool : True or False
+
+    """
+    if not all(abs(x - y) < tolerance for x, y in zip(a, b)):
+        return False
+    return True
+
+
 def iter_uri(path, sep):
     """Iter parents of node from its long name.
 
