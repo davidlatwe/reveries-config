d0a139cf4351c078f3163be28395ede6c2d6f19a
davidlatwe <david962041@gmail.com>
2018-10-04 19:21:27 +0800

    Remove sets loader forwarding

--- plugins/maya/load/load_sets.py
+++ plugins/maya/load/load_sets.py
@@ -29,7 +29,6 @@ class SetsLoader(avalon.api.Loader):
         import avalon.api
         import avalon.maya
         from reveries.maya.plugins import ReferenceLoader
-        from maya import cmds
 
         representation = context["representation"]
 
@@ -40,55 +39,6 @@ class SetsLoader(avalon.api.Loader):
         Loader = next(L for L in Loaders if issubclass(L, ReferenceLoader))
         loader = Loader(context)
 
-        asset = context['asset']
-        namespace = namespace or avalon.maya.lib.unique_namespace(
-            asset["name"] + "_",
-            prefix="_" if asset["name"][0].isdigit() else "",
-            suffix="_",
-        )
-
-        options.update({"post_process": False, "useSelection": True})
         container = loader.load(context, name, namespace, options)
 
-        cmds.addAttr(container, longName="sourceLoader", dataType="string")
-
-        cmds.setAttr(container + ".sourceLoader",
-                     cmds.getAttr(container + ".loader"),
-                     type="string")
-        cmds.setAttr(container + ".loader",
-                     self.__class__.__name__,
-                     type="string")
-
         return container
-
-    def _get_source_loader(self, container):
-
-        import avalon.api
-
-        for Loader in avalon.api.discover(avalon.api.Loader):
-            if Loader.__name__ == container["sourceLoader"]:
-                return Loader
-
-    def update(self, container, representation):
-
-        import avalon.pipeline
-
-        Loader = self._get_source_loader(container)
-        context = avalon.pipeline.get_representation_context(representation)
-        loader = Loader(context)
-
-        return loader.update(container, representation)
-
-    def switch(self, container, representation):
-        self.update(container, representation)
-
-    def remove(self, container):
-
-        import avalon.pipeline
-
-        Loader = self._get_source_loader(container)
-        representation = container["representation"]
-        context = avalon.pipeline.get_representation_context(representation)
-        loader = Loader(context)
-
-        return loader.remove(container)
