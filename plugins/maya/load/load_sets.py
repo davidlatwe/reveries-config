c6fce81f686b3ab16450141b93bcbb694d31d6fd
davidlatwe <david962041@gmail.com>
2018-10-04 23:37:11 +0800

    Implement set placing, place set to camera CoI

--- plugins/maya/load/load_sets.py
+++ plugins/maya/load/load_sets.py
@@ -40,4 +40,36 @@ class SetsLoader(avalon.api.Loader):
 
         container = loader.load(context, name, namespace, options)
 
+        self._place_set(container)
+
         return container
+
+    def _place_set(self, container):
+        from maya import cmds
+        from reveries.maya.plugins import parse_group_from_container
+
+        group = parse_group_from_container(container)
+        location = self._camera_coi()
+
+        if location is not None:
+            for attr, value in zip((".tx", ".ty", ".tz"), location):
+                cmds.setAttr(group + attr, value)
+
+        cmds.select(group)
+
+    def _camera_coi(self):
+        import math
+        from maya import cmds
+        from reveries.maya.lib import parse_active_camera
+
+        try:
+            camera = parse_active_camera()
+        except RuntimeError:
+            return None
+
+        COI = cmds.camera(camera, query=True, worldCenterOfInterest=True)
+
+        if any(math.isnan(val) for val in COI):
+            return None
+
+        return COI
