531eab7f99db4f7ee636d157278e87f2199965a1
davidlatwe <david962041@gmail.com>
2018-10-06 02:47:18 +0800

    Extract mesh hash value and keep shader for GPUCache

--- plugins/maya/publish/extract_model.py
+++ plugins/maya/publish/extract_model.py
@@ -6,7 +6,7 @@ import pyblish.api
 import maya.cmds as cmds
 import avalon.maya as maya
 
-from reveries.maya import capsule, io
+from reveries.maya import capsule, io, utils
 from reveries.plugins import PackageExtractor
 
 
@@ -30,14 +30,11 @@ class ExtractModel(PackageExtractor):
     ]
 
     def extract(self):
-        mesh_nodes = cmds.ls(self.member, type='mesh', ni=True, long=True)
-        clay_shader = "initialShadingGroup"
 
         with contextlib.nested(
             capsule.no_undo(),
             capsule.no_display_layers(self.member),
             capsule.no_smooth_preview(),
-            capsule.assign_shader(mesh_nodes, shadingEngine=clay_shader),
             maya.maintained_selection(),
             maya.without_extension(),
         ):
@@ -48,24 +45,37 @@ class ExtractModel(PackageExtractor):
         package_path = self.create_package(entry_file)
         entry_path = os.path.join(package_path, entry_file)
 
+        mesh_nodes = cmds.ls(self.member, type="mesh", ni=True, long=True)
+        clay_shader = "initialShadingGroup"
+
+        # Hash model
+        hasher = utils.MeshHasher()
+        for mesh in mesh_nodes:
+            hasher.add_mesh(mesh)
+        hash_val = hasher.hash()
+
+        self.add_data({"meshHash": hash_val})
+
         # Perform extraction
         self.log.info("Extracting %s" % str(self.member))
         cmds.select(self.member, noExpand=True)
-        cmds.file(
-            entry_path,
-            force=True,
-            typ="mayaBinary",
-            exportSelected=True,
-            preserveReferences=False,
-            # Shader assignment is the responsibility of
-            # riggers, for animators, and lookdev, for
-            # rendering.
-            shader=False,
-            # Construction history inherited from collection
-            # This enables a selective export of nodes
-            # relevant to this particular plug-in.
-            constructionHistory=False
-        )
+
+        with capsule.assign_shader(mesh_nodes, shadingEngine=clay_shader):
+            cmds.file(
+                entry_path,
+                force=True,
+                typ="mayaBinary",
+                exportSelected=True,
+                preserveReferences=False,
+                # Shader assignment is the responsibility of
+                # riggers, for animators, and lookdev, for
+                # rendering.
+                shader=False,
+                # Construction history inherited from collection
+                # This enables a selective export of nodes
+                # relevant to this particular plug-in.
+                constructionHistory=False
+            )
 
         self.log.info("Extracted {name} to {path}".format(
             name=self.data["subset"],
