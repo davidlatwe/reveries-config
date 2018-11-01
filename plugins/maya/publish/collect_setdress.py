54f2d58a19db0cd23815f6a75f4d5d1a0973c84b
davidlatwe <david962041@gmail.com>
2018-11-02 01:13:16 +0800

    Update setdress collector

--- plugins/maya/publish/collect_setdress.py
+++ plugins/maya/publish/collect_setdress.py
@@ -1,5 +1,6 @@
 
 import pyblish.api
+import maya.cmds as cmds
 from reveries.maya.plugins import ls_interfaces, get_group_from_interface
 
 
@@ -14,8 +15,9 @@ class CollectSetDress(pyblish.api.InstancePlugin):
 
     def process(self, instance):
 
-        set_roots = list()
-        pkg_data = dict()
+        set_roots = list()   # subsets' reference group node's parent node
+        set_groups = list()  # subsets' reference group node
+        inst_data = dict()
 
         for interface in ls_interfaces():
             vessel = get_group_from_interface(interface["objectName"])
@@ -23,12 +25,37 @@ class CollectSetDress(pyblish.api.InstancePlugin):
             if vessel in instance:
                 self.log.info("Collecting {!r} ..".format(vessel))
 
-                set_roots.append(vessel)
+                set_groups.append(vessel)
 
                 repr_id = interface["representation_id"]
-                if repr_id not in pkg_data:
-                    pkg_data[repr_id] = list()
-                pkg_data[repr_id].append(interface)
+                if repr_id not in inst_data:
+                    inst_data[repr_id] = {
+                        "loader": interface["loader"],
+                        "instances": list(),
+                    }
+
+                root = cmds.listRelatives(vessel, parent=True, fullPath=True)
+                root = (root or [None])[0]
+                matrix = cmds.xform(vessel,
+                                    query=True,
+                                    matrix=True,
+                                    objectSpace=True)
+
+                # The namespace stored in interface was absolute name,
+                # need to save as relative name.
+                # e.g. ":awesome" -> "awesome"
+                namespace = interface["namespace"][1:]
+
+                data = {
+                    "namespace": namespace,
+                    "root": root,
+                    "matrix": matrix,
+                }
+
+                if root not in set_roots:
+                    set_roots.append(root)
+                inst_data[repr_id]["instances"].append(data)
 
         instance.data["setdressRoots"] = set_roots
-        instance.data["packageData"] = pkg_data
+        instance.data["setdressGroups"] = set_groups
+        instance.data["instancesData"] = inst_data
