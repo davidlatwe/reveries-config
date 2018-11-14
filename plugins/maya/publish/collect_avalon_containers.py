787cffb78539033d6f0aa694bea91f0563cbd296
David Lai <david962041@gmail.com>
2018-11-14 19:23:30 +0800

    Add container collector

---
+++ plugins/maya/publish/collect_avalon_containers.py
@@ -0,0 +1,28 @@
+
+import pyblish.api
+import avalon.maya
+
+
+class CollectAvalonContainers(pyblish.api.ContextPlugin):
+
+    order = pyblish.api.CollectorOrder - 0.25
+    hosts = ["maya"]
+    label = "Avalon Containers"
+
+    def process(self, context):
+        root_containers = dict()
+        sub_containers = dict()
+
+        for container in avalon.maya.ls():
+
+            key = container["objectName"]
+
+            if container.get("parent"):
+                sub_containers[key] = container
+                self.log.debug("Sub Container: {}".format(key))
+            else:
+                root_containers[key] = container
+                self.log.info("Root Container: {}".format(key))
+
+        context.data["RootContainers"] = root_containers
+        context.data["SubContainers"] = sub_containers
