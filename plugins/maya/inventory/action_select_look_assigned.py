57a19f70b77abe9d93e4c992291fec758b8f45d1
David Lai <david962041@gmail.com>
2018-11-23 21:10:17 +0800

    Add select look assigned

---
+++ plugins/maya/inventory/action_select_look_assigned.py
@@ -0,0 +1,45 @@
+
+import avalon.api
+import avalon.maya
+from maya import cmds
+
+from reveries.maya.capsule import maintained_selection
+
+
+class SelectAssigned(avalon.api.InventoryAction):
+
+    label = "Select Assigned"
+    icon = "hand-o-right"
+    color = "#d8d8d8"
+
+    @staticmethod
+    def is_compatible(container):
+        return container.get("loader") == "LookLoader"
+
+    def process(self, containers):
+
+        shaders = set()
+        for container in containers:
+            if not container.get("loader") == "LookLoader":
+                continue
+
+            # Select assigned
+            members = cmds.sets(container["objectName"], query=True)
+            shaders.update(cmds.ls(members, type="shadingEngine"))
+
+        with maintained_selection():
+            cmds.select(list(shaders), replace=True)
+
+            # Parse selected
+            containers = avalon.maya.ls()
+            container_names = set(c["objectName"] for c in containers)
+
+            selected_items = set()
+            for node in cmds.ls(sl=True):
+                objsets = cmds.listSets(object=node) or []
+                for objset in objsets:
+                    if objset in container_names:
+                        selected_items.add(objset)
+                        break
+
+        return selected_items
