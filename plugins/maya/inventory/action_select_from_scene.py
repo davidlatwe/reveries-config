a9859ecd6cf54e35ff1011684e5635d5a1439246
David Lai <david962041@gmail.com>
2018-11-23 20:20:44 +0800

    Add action to select items in Inventory App from scene

---
+++ plugins/maya/inventory/action_select_from_scene.py
@@ -0,0 +1,36 @@
+
+import avalon.api
+import avalon.maya
+from maya import cmds
+
+
+class SelectFromScene(avalon.api.InventoryAction):
+
+    label = "Select From Scene"
+    icon = "hand-o-down"
+    color = "#d8d8d8"
+
+    @staticmethod
+    def is_compatible(container):
+        return True
+
+    def process(self, containers):
+
+        containers = avalon.maya.ls()
+        container_names = list(c["objectName"] for c in containers)
+
+        selected = cmds.ls(sl=True)
+        selected_items = set()
+
+        for node in selected:
+            if node in container_names:
+                selected_items.add(node)
+                continue
+
+            objsets = cmds.listSets(object=node) or []
+            for objset in objsets:
+                if objset in container_names:
+                    selected_items.add(objset)
+                    break
+
+        return selected_items
