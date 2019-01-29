35d3e7bd30fa06824ff7d5a7723a3d2429dfbc07
David Lai <davidlatwe@gmail.com>
2019-01-29 17:27:59 +0800

    Lock all nodes in Maya scene after publish

---
+++ plugins/maya/publish/avalon_lock_nodes.py
@@ -0,0 +1,24 @@
+
+import pyblish.api
+
+
+class AvalonLockNodes(pyblish.api.ContextPlugin):
+    """Flush undo queue and remove the modified state of the entire scene
+    """
+
+    label = "Flush Modification"
+    order = pyblish.api.IntegratorOrder + 0.499999
+    hosts = ["maya"]
+
+    def process(self, context):
+        from reveries.maya.pipeline import lock_edit, is_editable
+
+        assert all(result["success"] for result in context.data["results"]), (
+            "Atomicity not held, aborting.")
+
+        if not context.data.get("_has_privileged_instance"):
+            return
+
+        assert is_editable(), "Scene already not editable, this is a bug."
+
+        lock_edit()
