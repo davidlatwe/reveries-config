a72675f8027e37b98811516e701f306563c9dff2
David Lai <davidlatwe@gmail.com>
2019-01-29 17:24:52 +0800

    Add Maya Arnold utils

---
+++ reveries/maya/arnold/__init__.py
@@ -0,0 +1,15 @@
+from maya import cmds
+
+from .utils import (
+    get_arnold_aov_nodes,
+    get_arnold_aov_names,
+)
+
+# Requirement, obviously
+cmds.loadPlugin('mtoa', quiet=True)
+
+
+__all__ = (
+    "get_arnold_aov_nodes",
+    "get_arnold_aov_names",
+)
