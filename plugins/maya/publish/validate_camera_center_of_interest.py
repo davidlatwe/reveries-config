c59ba3f38a5016fa91445e493da8e04e490dea73
David Lai <david962041@gmail.com>
2019-01-30 01:24:22 +0800

    Improve camera validators

--- plugins/maya/publish/validate_camera_center_of_interest.py
+++ plugins/maya/publish/validate_camera_center_of_interest.py
@@ -8,19 +8,29 @@ class ValidateCameraCenterOfInterest(pyblish.api.InstancePlugin):
     """Ensure camera's `centerOfInterest` is not NaN
     """
 
-    order = pyblish.api.ValidatorOrder + 0.1
+    order = pyblish.api.ValidatorOrder
     hosts = ["maya"]
     label = "Center Of Interest"
     families = [
+        "reveries.imgseq",
         "reveries.camera",
     ]
 
+    @classmethod
+    def get_invalid(cls, instance):
+        invalid = list()
+
+        cameras = cmds.ls(instance[:], type="camera", long=True)
+        for cam in cameras:
+            coi_value = cmds.getAttr(cam + ".centerOfInterest")
+            if math.isnan(coi_value):
+                invalid.append(cam)
+
+        return invalid
+
     def process(self, instance):
-        try:
-            camera = cmds.ls(instance[:], type="camera")[0]
-        except IndexError:
-            raise Exception("No camera.")
-
-        coi_value = cmds.getAttr(camera + ".centerOfInterest")
-
-        assert not math.isnan(coi_value), ("Camera `centerOfInterest` is NaN.")
+        invalid = self.get_invalid(instance)
+        if invalid:
+            self.log.error("Camera `centerOfInterest` is NaN.")
+            raise RuntimeError("Invalid cameras "
+                               "found: {0}".format(invalid))
