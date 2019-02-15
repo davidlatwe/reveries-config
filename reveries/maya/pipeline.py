fdbba730ac0fd09b3f804370adaca67389748a00
David Lai <davidlatwe@gmail.com>
2019-02-15 15:14:17 +0800

    Move `env_embedded_path` to `maya.pipeline`

--- reveries/maya/pipeline.py
+++ reveries/maya/pipeline.py
@@ -66,6 +66,23 @@ def unlock_edit():
     reset_edit_lock()
 
 
+def env_embedded_path(path):
+    """Embed environment var `$AVALON_PROJECTS` and `$AVALON_PROJECT` into path
+
+    This will ensure reference or cache path resolvable when project root
+    moves to other place.
+
+    """
+    path = path.replace(
+        avalon.api.registered_root(), "$AVALON_PROJECTS", 1
+    )
+    path = path.replace(
+        avalon.Session["AVALON_PROJECT"], "$AVALON_PROJECT", 1
+    )
+
+    return path
+
+
 def subset_group_name(namespace, name):
     return "{}:{}".format(namespace, name)
 
--- reveries/maya/plugins.py
+++ reveries/maya/plugins.py
@@ -25,6 +25,7 @@ from .pipeline import (
     subset_containerising,
     unique_root_namespace,
     update_container,
+    env_embedded_path,
 )
 
 from .hierarchy import (
@@ -58,23 +59,6 @@ def load_plugin(representation):
         pass
     else:
         cmds.loadPlugin(plugin, quiet=True)
-
-
-def env_embedded_path(path):
-    """Embed environment var `$AVALON_PROJECTS` and `$AVALON_PROJECT` into path
-
-    This will ensure reference or cache path resolvable when project root
-    moves to other place.
-
-    """
-    path = path.replace(
-        avalon.api.registered_root(), "$AVALON_PROJECTS", 1
-    )
-    path = path.replace(
-        avalon.Session["AVALON_PROJECT"], "$AVALON_PROJECT", 1
-    )
-
-    return path
 
 
 class MayaBaseLoader(PackageLoader):
