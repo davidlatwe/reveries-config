90b86eaaec444c6a4c1655474dfeaac4bff1aabc
David Lai <davidlatwe@gmail.com>
2019-02-17 18:03:56 +0800

    Add model swap tool for lookDev

--- reveries/maya/interactive.py
+++ reveries/maya/interactive.py
@@ -1,7 +1,10 @@
 
 from maya import cmds
 
+from avalon import api, io
+
 from .vendor import capture
+from . import lib, pipeline
 
 
 def active_view_snapshot(*args):
@@ -35,3 +38,64 @@ def wipe_all_namespaces():
                            mergeNamespaceWithRoot=True)
         except RuntimeError:
             pass
+
+
+def swap_to_published_model(*args):
+    """Hide the working model and load the published version of it
+
+    This is for the case that artist was working on model and lookDev all
+    together, while publishing turntable require the model to be published
+    version.
+
+    Using this tool could load the latest version via the instance that was
+    used to publish this model.
+
+    """
+    MSG = "Please select '|ROOT' node, and '|ROOT' node only."
+
+    selection = cmds.ls(selection=True, long=True, type="transform")
+    assert len(selection) == 1, MSG
+
+    root = selection[0]
+    assert root.endswith("|ROOT"), MSG
+
+    instances = lib.lsAttrs({"id": "pyblish.avalon.instance",
+                             "family": "reveries.model"})
+
+    project = api.Session.get("AVALON_PROJECT")
+    asset = None
+    subset = None
+    for set_ in cmds.listSets(object=root) or []:
+        if set_ in instances:
+            asset = cmds.getAttr(set_ + ".asset")
+            subset = cmds.getAttr(set_ + ".subset")
+            break
+
+    assert project is not None, "Project undefined, this is not right."
+    assert asset and subset, "Model instance not found."
+    assert len(instances) == 1, "Too many model instances in scene."
+
+    representation = io.locate([project, asset, subset, -1, "mayaBinary"])
+
+    Loaders = api.discover(api.Loader)
+    Loader = next((loader for loader in Loaders
+                   if loader.__name__ == "ModelLoader"), None)
+
+    assert Loader is not None, "ModelLoader not found, this is a bug."
+    assert representation is not None, "Representation not found."
+
+    container = api.load(Loader, representation)
+
+    group = pipeline.get_group_from_container(container["objectName"])
+
+    parent = cmds.listRelatives(root, parent=True)
+    if parent:
+        cmds.parent(group, parent)
+
+    # Re-assign shaders
+    nodes = cmds.listRelatives(root, allDescendents=True, fullPath=True)
+    shader_by_id = lib.serialise_shaders(nodes)
+    lib.apply_shaders(shader_by_id)
+
+    # Hide unpublished model
+    cmds.setAttr(root + ".visibility", False)
