6535ce45068fbc0031d1083d0c7088d8a09973ff
David Lai <davidlatwe@gmail.com>
2019-02-18 03:10:38 +0800

    Update renderable camera validator

--- plugins/maya/publish/validate_camera_renderable.py
+++ plugins/maya/publish/validate_camera_renderable.py
@@ -2,35 +2,20 @@
 import pyblish.api
 
 
-class ValidateRenderableCamera(pyblish.api.InstancePlugin):
+class ValidateSingleRenderableCamera(pyblish.api.InstancePlugin):
     """Ensure the instance content only renderable camera
     """
 
     order = pyblish.api.ValidatorOrder
     hosts = ["maya"]
-    label = "Renderable Camera"
+    label = "Single Renderable Camera"
     families = [
-        "reveries.imgseq.batchrender",
-        "reveries.imgseq.turntable",
+        "reveries.imgseq",
     ]
-
-    @classmethod
-    def get_invalid(cls, instance):
-        from maya import cmds
-
-        invalid = []
-
-        cameras = cmds.ls(instance[:], type="camera", long=True)
-        for cam in cameras:
-            if cam not in instance.data["renderCam"]:
-                invalid.append(cam)
-
-        return invalid
 
     def process(self, instance):
         if len(instance.data["renderCam"]) == 0:
             raise Exception("No renderable camera.")
 
-        invalid = self.get_invalid(instance)
-        if invalid:
-            raise Exception("Camera not renderable.")
+        if len(instance.data["renderCam"]) > 1:
+            raise Exception("Can only have one renderable camera.")
