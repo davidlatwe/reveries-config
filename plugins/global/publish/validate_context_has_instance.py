345939606c81ca18ac5ba92137488b36002cbd2a
David Lai <davidlatwe@gmail.com>
2019-02-18 23:54:37 +0800

    Improve log

--- plugins/global/publish/validate_context_has_instance.py
+++ plugins/global/publish/validate_context_has_instance.py
@@ -15,6 +15,7 @@ class ValidateContextHasInstance(pyblish.api.ContextPlugin):
 
     def process(self, context):
         msg = "No instance to publish, please create at least one instance."
-
         assert len(context), msg
+
+        msg = "No instance to publish, please enable at least one instance."
         assert any(inst.data.get("publish", True) for inst in context), msg
