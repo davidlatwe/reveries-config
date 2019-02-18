f923d72ebb3072ab321614b94cb73768cd3d4bc0
David Lai <davidlatwe@gmail.com>
2019-02-19 00:13:53 +0800

    Add error raiser to trigger job fail

--- plugins/global/contractor/scripts/avalon_contractor_publish.py
+++ plugins/global/contractor/scripts/avalon_contractor_publish.py
@@ -8,33 +8,23 @@ will be local path which highly possible not be able to access by Deadline
 slaves.
 
 """
-import copy
 import logging
 import pyblish.api
 import pyblish.util
 
-from bson import json_util
 from reveries.utils import publish_results_formatting
 from reveries.plugins import parse_contract_environment
 
 
 log = logging.getLogger("Contractor")
 
-
-def show_data(context):
-    data = copy.deepcopy(context.data)
-    data["results"] = publish_results_formatting(context)
-    log.info(json_util.dumps(data, indent=4, sort_keys=True))
+error_raiser = None
 
 
 def check_success(context):
     for result in publish_results_formatting(context):
         if not result["success"]:
-            show_data(context)
-            log.error(result["plugin"]["name"])
-            log.error(json_util.dumps(result["error"],
-                                      indent=4, sort_keys=True))
-            raise RuntimeError(result["error"]["message"])
+            error_raiser("Publish failed.")
 
 
 def publish():
@@ -42,11 +32,13 @@ def publish():
     context = pyblish.api.Context()
 
     log.info("Parsing environment ...")
-    parse_contract_environment(context)
+    try:
+        parse_contract_environment(context)
+    except Exception as e:
+        error_raiser(str(e))
 
     log.info("Collecting instances ...")
     pyblish.util.collect(context)
-    check_success(context)
 
     log.info("Validating ...")
     pyblish.util.validate(context)
@@ -54,7 +46,6 @@ def publish():
 
     log.info("Extracting ...")
     pyblish.util.extract(context)
-    check_success(context)
 
     log.info("Integrating ...")
     pyblish.util.integrate(context)
@@ -63,5 +54,33 @@ def publish():
     log.info("Completed.")
 
 
+def maya_error_raiser():
+    """Return a Deadline Maya faild job trigger"""
+    from maya import cmds
+
+    def _maya_error_raiser(msg):
+        """Trigger Deadline Maya Plugin to faild job
+        """
+        # "Render failed:" is one of the magic string to trigger faild job.
+        # Those magic string were listed in Deadline plugin `MayaBatch.py`
+        cmds.error("Render failed: " + msg)
+
+    return _maya_error_raiser
+
+
 if __name__ == "__main__":
+    # Pick a job error raiser before publish
+    RAISERS = [
+        maya_error_raiser,
+    ]
+    for raiser in RAISERS:
+        try:
+            error_raiser = raiser()
+        except ImportError:
+            pass
+        else:
+            break
+    else:
+        error_raiser = log.error
+
     publish()
