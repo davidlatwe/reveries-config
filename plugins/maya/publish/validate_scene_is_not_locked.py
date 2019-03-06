0dc492c4a6cb13a2548ce28fbc624a27ee4c9488
David Lai <david962041@gmail.com>
2019-03-07 06:16:23 +0800

    Add scene lock validator as a headsup for artist

---
+++ plugins/maya/publish/validate_scene_is_not_locked.py
@@ -0,0 +1,20 @@
+
+import pyblish.api
+from avalon import maya
+
+
+class ValidateSceneNotLocked(pyblish.api.ContextPlugin):
+    """Nothing can be published if scene is locked
+    """
+
+    label = "Scene Is Not Locked"
+    order = pyblish.api.ValidatorOrder - 0.4999
+
+    def process(self, context):
+
+        if context.data.get("contractorAccepted"):
+            return
+
+        if maya.is_locked():
+            raise Exception("Scene has been locked, please save the scene "
+                            "with another name.")
