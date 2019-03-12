4d9825815406dc02775c593f8ec66f5597c045c0
David Lai <davidlatwe@gmail.com>
2019-03-13 00:03:25 +0800

    Support extra resolution format on turntable rendering

--- plugins/maya/publish/validate_render_resolution.py
+++ plugins/maya/publish/validate_render_resolution.py
@@ -1,5 +1,6 @@
 
 import pyblish.api
+from avalon import io
 from reveries.maya import utils as maya_utils
 from reveries.maya import pipeline
 from reveries import utils
@@ -23,17 +24,55 @@ class ValidateRenderResolution(pyblish.api.InstancePlugin):
     @classmethod
     def get_invalid(cls, instance):
         """Rendering resolution should be the same as project settings"""
+        valid_resolutions = list()
+
         project = instance.context.data["projectDoc"]
-        asset_name = pipeline.has_turntable()
+        is_turntable = pipeline.has_turntable()
+
+        turntable = project["data"]["pipeline"]["maya"].get("turntable")
+        if turntable is not None and turntable == is_turntable:
+            # (NOTE) When publishing turntable, some asset may need other
+            #        render resolution width/height to fit in.
+            #
+            #        To add extra valid resolution for asset, add this to
+            #        the turntable asset data:
+            #
+            #           "exceptions" : {
+            #               "assetName": [
+            #                   [width1, height1],
+            #                   [width2, height2],
+            #                   ...
+            #                ]
+            #            }
+            #
+            # (TODO) Might need to think a better way to implement this.
+            #
+            ttable_doc = io.find_one({"type": "asset",
+                                      "name": turntable})
+            ttable_data = ttable_doc["data"]
+            exception = ttable_data["exceptions"].get(instance.data["asset"])
+            if exception is not None:
+                valid_resolutions += exception.get("resolution", [])
+
         proj_width, proj_height = utils.get_resolution_data(project,
-                                                            asset_name)
+                                                            is_turntable)
+        valid_resolutions.append((proj_width, proj_height))
+
         layer = instance.data["renderlayer"]
         scene_width, scene_height = maya_utils.get_render_resolution(layer)
 
-        if proj_width != scene_width or proj_height != scene_height:
+        invalid = True
+        for res in valid_resolutions:
+            if res[0] == scene_width or res[1] == scene_height:
+                invalid = False
+                break
+
+        if invalid:
             cls.log.error("Resolution width and height should be {0} x {1}."
-                          "".format(proj_width, proj_height))
-            return True
+                          "".format(valid_resolutions.pop()))
+            for res in valid_resolutions:
+                cls.log.error("Or {0} x {1}".format(res))
+        return invalid
 
     def process(self, instance):
         self.log.info("Validating image resolution..")
