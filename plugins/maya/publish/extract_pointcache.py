7c861bdc636aef3d9e3e2bd562bf99cd8b377c02
David Lai <davidlatwe@gmail.com>
2019-04-30 22:18:07 +0800

    plugins.maya: Resolve duplicate named error while exporting Alembic

--- plugins/maya/publish/extract_pointcache.py
+++ plugins/maya/publish/extract_pointcache.py
@@ -56,22 +56,40 @@ class ExtractPointCache(DelegatablePackageExtractor):
 
         root = cmds.ls(sl=True, long=True)
 
-        io.export_alembic(
-            cache_path,
-            self.start_frame,
-            self.end_frame,
-            selection=False,
-            renderableOnly=True,
-            writeCreases=True,
-            worldSpace=True,
-            root=root,
-            attr=[
-                lib.AVALON_ID_ATTR_LONG,
-            ],
-            attrPrefix=[
-                "ai",  # Write out Arnold attributes
-            ],
-        )
+        with capsule.maintained_selection():
+            # Selection may change if there are duplicate named nodes and
+            # require instancing them to resolve
+
+            with capsule.delete_after() as delete_bin:
+
+                # (NOTE) We need to check any duplicate named nodes, or
+                #        error will raised during Alembic export.
+                result = lib.ls_duplicated_name(root)
+                duplicated = [n for m in result.values() for n in m]
+                if duplicated:
+                    # Create instance to avoid duplicated names
+                    instanced = cmds.instance(duplicated)
+                    # Instance nodes will be deleted after the export
+                    delete_bin.extend(instanced)
+                    # Update root nodes
+                    root = list(set(root) - set(duplicated)) + instanced
+
+                io.export_alembic(
+                    cache_path,
+                    self.start_frame,
+                    self.end_frame,
+                    selection=False,
+                    renderableOnly=True,
+                    writeCreases=True,
+                    worldSpace=True,
+                    root=root,
+                    attr=[
+                        lib.AVALON_ID_ATTR_LONG,
+                    ],
+                    attrPrefix=[
+                        "ai",  # Write out Arnold attributes
+                    ],
+                )
 
         io.wrap_abc(entry_path, [(cache_file, "ROOT")])
 
