dae67a9dc85a06e8610f4adc2740841411d53ed3
David Lai <davidlatwe@gmail.com>
2019-07-16 23:49:22 +0800

    plugins.maya: Refactored arnold stand-in extractor

--- plugins/maya/publish/extract_arnold_standin.py
+++ plugins/maya/publish/extract_arnold_standin.py
@@ -1,57 +1,13 @@
 
 import os
 import contextlib
+from collections import OrderedDict
 
 import pyblish.api
 from reveries.plugins import PackageExtractor, skip_stage
 from reveries.maya import capsule
 
 from maya import cmds
-
-
-@contextlib.contextmanager
-def unlock_colorspace(node):
-    """
-    """
-    color_attr = node + ".colorSpace"
-    color_space = cmds.getAttr(color_attr)
-    cmds.setAttr(color_attr, lock=False)
-    try:
-        yield
-    finally:
-        cmds.setAttr(color_attr, color_space, lock=False, type="string")
-
-
-@contextlib.contextmanager
-def remove_file_env_path(data):
-    """
-    """
-    root = data["relativeRoot"]
-    replace = data["replaceRoot"]
-
-    for node in data["fileNodes"]:
-        # Must be starts with `root`, validated.
-        origin_path = cmds.getAttr(node + ".fileTextureName")
-        for root_key, replace_key in zip(root, replace):
-            origin_path = origin_path.replace(root_key, replace_key)
-        arnold_path = origin_path
-
-        with unlock_colorspace(node):
-            cmds.setAttr(node + ".fileTextureName",
-                         arnold_path,
-                         type="string")
-    try:
-        yield
-    finally:
-        for node in data["fileNodes"]:
-            arnold_path = cmds.getAttr(node + ".fileTextureName")
-            for root_key, replace_key in zip(root, replace):
-                arnold_path = arnold_path.replace(replace_key, root_key)
-            origin_path = arnold_path
-            with unlock_colorspace(node):
-                cmds.setAttr(node + ".fileTextureName",
-                             origin_path,
-                             type="string")
 
 
 class ExtractArnoldStandIn(PackageExtractor):
@@ -78,13 +34,30 @@ class ExtractArnoldStandIn(PackageExtractor):
         cache_file = self.file_name("ass")
         cache_path = os.path.join(package_path, cache_file)
 
+        root = self.data["relativeRoot"]
+        replace = self.data["replaceRoot"]
+
+        attr_values = OrderedDict()
+
+        for node in self.data["fileNodes"]:
+            attr = node + ".fileTextureName"
+            # Must be starts with `root`, validated.
+            path = cmds.getAttr(attr)
+            for root_key, replace_key in zip(root, replace):
+                path = path.replace(root_key, replace_key)
+            attr_values[attr] = path
+
+            attr = node + ".colorSpace"
+            attr_values[attr] = cmds.getAttr(attr)
+
         with contextlib.nested(
             capsule.no_undo(),
             capsule.no_refresh(),
             capsule.evaluation("off"),
             capsule.maintained_selection(),
             capsule.ref_edit_unlock(),
-            remove_file_env_path(self.data),
+            capsule.attribute_states(attr_values.keys(), lock=False),
+            capsule.attribute_values(attr_values),
         ):
             cmds.select(self.member, replace=True)
             asses = cmds.arnoldExportAss(filename=cache_path,
