9308435640a4707fd255ae2e57452ea79e24de6e
David Lai <davidlatwe@gmail.com>
2019-09-06 17:41:05 +0800

    plugins.maya: Improve #161

---
+++ plugins/maya/publish/collect_all_instanced.py
@@ -0,0 +1,32 @@
+import pyblish.api
+from maya import cmds
+
+
+class CollectAllInstanced(pyblish.api.InstancePlugin):
+    """Collect instanced objects from Maya instancers
+    """
+
+    order = pyblish.api.CollectorOrder + 0.115
+    hosts = ["maya"]
+    label = "Collect All Instanced"
+
+    def process(self, instance):
+
+        instanced_hierarchies = dict()
+        instanced_roots = set()
+
+        for instancer in cmds.ls(instance, type="instancer"):
+            inputs = cmds.listConnections(instancer + ".inputHierarchy",
+                                          source=True,
+                                          destination=False)
+            instanced_roots.update(cmds.ls(inputs, long=True))
+
+        for root in instanced_roots:
+            hierarchy = [root]
+            hierarchy += cmds.listRelatives(hierarchy,
+                                            allDescendents=True,
+                                            fullPath=True) or []
+
+            instanced_hierarchies[root] = sorted(set(hierarchy))
+
+        instance.data["instancedHierarchies"] = instanced_hierarchies
