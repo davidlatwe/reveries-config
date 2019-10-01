9db041117d117a63a4599c84ff6c98fbe1e607c8
David Lai <davidlatwe@gmail.com>
2019-10-01 20:00:49 +0800

    plugins.maya: Change to spawn pointcache instance directly via OutSet

--- plugins/maya/publish/collect_deformed_outputs.py
+++ plugins/maya/publish/collect_deformed_outputs.py
@@ -8,21 +8,19 @@ from reveries.maya import lib, pipeline
 class CollectDeformedOutputs(pyblish.api.InstancePlugin):
     """Collect out geometry data for instance.
 
-    If node's visibility is off, will not be cached, but hidden by
-    displayLayer will.
+    Only visible objects will be cached.
 
-    If the subset variant is "default", collect deformable nodes from
-    objectSets of the loaded subset, which name is or endswith "OutSet",
-    and create instances from them. For "OutSet" that has prefix, will
-    use that prefix as variant of subset.
+    If the caching source has any objectSet which name is or endswith
+    "OutSet", will create instances from them. For "OutSet" that has
+    prefix, will use that prefix as variant of subset.
 
     For example:
              "OutSet" -> "pointcache.Boy_model_01_Default"
           "SimOutSet" -> "pointcache.Boy_model_01_Sim"
         "ClothOutSet" -> "pointcache.Boy_model_01_Cloth"
 
-    If the subset variant is Not "default", collect deformable node
-    from instance member (selection).
+    If no "OutSet", collect deformable nodes directly from instance
+    member (selection).
 
     """
 
@@ -35,9 +33,6 @@ class CollectDeformedOutputs(pyblish.api.InstancePlugin):
 
     def process(self, instance):
 
-        variant = instance.data["subset"][len("pointcache"):].lower()
-        members = instance[:]
-
         # Frame range
         if instance.data["staticCache"]:
             start_frame = cmds.currentTime(query=True)
@@ -47,21 +42,29 @@ class CollectDeformedOutputs(pyblish.api.InstancePlugin):
             start_frame = get({"minTime": True})
             end_frame = get({"maxTime": True})
 
-        if variant == "default":
-            # Collect cacheable nodes from OutSet of loaded subset
+        members = instance[:]
+        out_sets = list()
+
+        # Find OutSet from subset group nodes
+        for group in cmds.ls(members, type="transform", long=True):
+            if cmds.listRelatives(group, shapes=True):
+                continue
+
+            try:
+                container = pipeline.get_container_from_group(group)
+            except AssertionError:
+                # Not a subset group node
+                continue
+
+            sets = cmds.ls(cmds.sets(container, query=True),
+                           type="objectSet")
+            out_sets += [s for s in sets if s.endswith("OutSet")]
+
+        # Collect cacheable nodes
+        if out_sets:
+            # Cacheables from OutSet of loaded subset
             out_cache = dict()
-            out_sets = list()
             created = False
-
-            for node in cmds.ls(members, type="transform", long=True):
-                try:
-                    container = pipeline.get_container_from_group(node)
-                except AssertionError:
-                    continue
-
-                sets = cmds.ls(cmds.sets(container, query=True),
-                               type="objectSet")
-                out_sets += [s for s in sets if s.endswith("OutSet")]
 
             for node in out_sets:
                 name = node.rsplit(":", 1)[-1][:-len("OutSet")] or "Default"
@@ -114,7 +117,7 @@ class CollectDeformedOutputs(pyblish.api.InstancePlugin):
                 context.remove(backup)
 
         else:
-            # Collect cacheable nodes from instance member
+            # Cacheables from instance member
             cacheables = lib.pick_cacheable(members)
             cacheables = lib.get_visible_in_frame_range(cacheables,
                                                         int(start_frame),
