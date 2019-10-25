9dc279a4b78253b6c4bf8131f19d5bcb19c33b3c
David Lai <davidlatwe@gmail.com>
2019-10-25 17:28:30 +0800

    First draft of AvalonId editor

---
+++ reveries/maya/tools/avalonideditor/view.py
@@ -0,0 +1,103 @@
+
+from avalon.vendor.Qt import QtWidgets, QtCore
+
+from . import model, delegate
+
+
+class SelectionOutline(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+        super(SelectionOutline, self).__init__(parent=parent)
+
+        self.model = model.SelectionModel()
+        proxy = QtCore.QSortFilterProxyModel()
+        proxy.setSourceModel(self.model)
+
+        view = QtWidgets.QTreeView()
+        view.setIndentation(20)
+        view.setStyleSheet("""
+            QTreeView::item{
+                padding: 4px 1px;
+                border: 0px;
+            }
+        """)
+        view.setAllColumnsShowFocus(True)
+        view.setAlternatingRowColors(True)
+        view.setSortingEnabled(True)
+        view.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        view.sortByColumn(1, QtCore.Qt.AscendingOrder)
+        view.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+
+        # Delegate
+        time_delegate = delegate.PrettyTimeDelegate()
+        column = self.model.Columns.index("time")
+        view.setItemDelegateForColumn(column, time_delegate)
+
+        view.setModel(proxy)
+        view.setColumnWidth(0, 100)
+        view.setColumnWidth(1, 140)
+        view.setColumnWidth(2, 200)
+        view.setColumnWidth(3, 200)
+        view.setColumnWidth(4, 70)
+        self.view = view
+        self.proxy = proxy
+
+        freezer = QtWidgets.QCheckBox("Freeze List")
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(freezer)
+        layout.addWidget(self.view)
+
+        self.setLayout(layout)
+
+        self.data = {
+            "delegates": {
+                "time": time_delegate,
+            }
+        }
+
+        self.view.customContextMenuRequested.connect(self.on_menu)
+        freezer.stateChanged.connect(self.on_freezed)
+
+    def start(self):
+        self.model.stop()
+        self.model.on_selected()
+        self.model.listen()
+
+    def stop(self):
+        self.model.stop()
+
+    def on_freezed(self, state):
+        self.model._selection_freezed = state
+
+    def on_menu(self, point):
+        point_index = self.view.indexAt(point)
+        if not point_index.isValid():
+            return
+
+        menu = QtWidgets.QMenu(self)
+
+        select_action = QtWidgets.QAction("Select..", menu)
+        select_action.triggered.connect(self.act_select)
+
+        menu.addAction(select_action)
+
+        # Show the context action menu
+        global_point = self.view.mapToGlobal(point)
+        # Tweak menu position so user can directly hit select action without
+        # moving cursor much.
+        global_point.setX(global_point.x() - 70)
+        global_point.setY(global_point.y() + 10)
+        action = menu.exec_(global_point)
+        if not action:
+            return
+
+    def act_select(self):
+        selection_model = self.view.selectionModel()
+        selection = selection_model.selection()
+        source_selection = self.proxy.mapSelectionToSource(selection)
+        self.model.select_back(source_selection.indexes())
+
+
+class Diagnose(QtWidgets.QWidget):
+    pass
