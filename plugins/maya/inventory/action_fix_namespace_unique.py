415f57ae6e17a6760a377f5cd53a8e8c7571da34
David Lai <davidlatwe@gmail.com>
2019-12-09 23:20:24 +0800

    Add namespace duplication fixing action

---
+++ plugins/maya/inventory/action_fix_namespace_unique.py
@@ -0,0 +1,108 @@
+
+import avalon.api
+import avalon.maya
+from avalon import io
+
+
+class FixNamespaceUnique(avalon.api.InventoryAction):
+    """Fix container to ensure namespace unique
+
+    Artist may import scene themselve which contain loaded subsets, and
+    create dupication.
+
+    This action aim to resolve namespace duplication only, duplicated
+    main containers is not responsible by this action.
+
+    """
+
+    label = "Fix Namespace Unique"
+    icon = "wrench"
+    color = "#F13A3A"
+    order = -100
+
+    @staticmethod
+    def is_compatible(container):
+        from maya import cmds
+        from avalon.maya.pipeline import AVALON_CONTAINERS
+        from reveries.maya import lib
+
+        if not container:
+            return False
+
+        main_containers = cmds.ls(AVALON_CONTAINERS[1:] + "*", recursive=True)
+        parents = cmds.listSets(object=container["objectName"]) or []
+        # Must be a root container
+        if any(main in parents for main in main_containers):
+            namespace = container["namespace"]
+            filter = {"id": "pyblish.avalon.container", "namespace": namespace}
+            if len(lib.lsAttrs(filter)) > 1:
+                return True
+        return False
+
+    def process(self, containers):
+        from maya import cmds
+        from reveries.maya import lib, pipeline
+        from avalon.tools import sceneinventory
+
+        cached_document = dict()
+
+        def get_document(id):
+            if id in cached_document:
+                doc = cached_document[id]
+            else:
+                doc = io.find_one({"_id": io.ObjectId(id)})
+                cached_document[id] = doc
+            return doc
+
+        for container in containers:
+            namespace = container["namespace"]
+            filter = {"id": "pyblish.avalon.container", "namespace": namespace}
+            if len(lib.lsAttrs(filter)) == 1:
+                # Namespace is unique
+                continue
+
+            # Create new namespace
+            asset = get_document(container["assetId"])
+            asset_name = asset["name"]
+
+            subset = get_document(container["subsetId"])
+            if subset["schema"] == "avalon-core:subset-3.0":
+                family = subset["data"]["families"][0]
+            else:
+                version = get_document(container["versionId"])
+                family = version["data"]["families"][0]
+            family_name = family.split(".")[-1]
+
+            new_namespace = pipeline.unique_root_namespace(
+                asset_name=asset_name,
+                family_name=family_name,
+            )
+
+            CON = container["objectName"]
+            members = cmds.ls(cmds.sets(CON, query=True, nodesOnly=True),
+                              long=True)
+            reference_node = lib.get_highest_reference_node(members)
+
+            if reference_node:
+                filename = cmds.referenceQuery(reference_node, filename=True)
+                cmds.file(filename, edit=True, namespace=new_namespace)
+            else:
+                cmds.namespace(add=new_namespace)
+
+            for node in members:
+                if not cmds.objExists(node):
+                    continue
+                if cmds.referenceQuery(node, isNodeReferenced=True):
+                    continue
+                if cmds.lockNode(node, query=True)[0]:
+                    continue
+
+                new = "|".join(p.replace(namespace[1:], new_namespace[1:], 1)
+                               for p in node.split("|"))
+                if node != new:
+                    cmds.rename(node, new)
+
+            cmds.setAttr(CON + ".namespace", new_namespace, type="string")
+            container["namespace"] = new_namespace
+
+        sceneinventory.app.window.refresh()
