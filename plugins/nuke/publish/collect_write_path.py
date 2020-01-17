31f2fc0a5b012b46ec790ada749237331449fff8
davidlatwe <davidlatwe@gmail.com>
2020-01-17 21:36:53 +0800

    Convert output path frame format string

--- plugins/nuke/publish/collect_write_path.py
+++ plugins/nuke/publish/collect_write_path.py
@@ -1,6 +1,20 @@
 
 import pyblish.api
+import re
 import os
+
+
+def convert_frame_format(path):
+    """Convert format like '%04d' into '####' padding format"""
+    head, tail = os.path.split(path)
+
+    def replace(match):
+        count = int(match.group(2))
+        return match.group(1) + "#" * count + match.group(3)
+
+    tail = re.sub("(.*)%([0-9]*)d(.*)", replace, tail)
+
+    return head + "/" + tail
 
 
 class CollectWritePath(pyblish.api.InstancePlugin):
@@ -16,5 +30,9 @@ class CollectWritePath(pyblish.api.InstancePlugin):
         write = instance[0]
         outpath = write["file"].value()
         outpath = os.path.normpath(outpath).replace("\\", "/")
+        outpath = convert_frame_format(outpath)
 
         instance.data["outputPath"] = outpath
+        instance.data["fileExt"] = os.path.splitext(outpath)[-1]
+
+        self.log.info("Write to: %s" % outpath)
