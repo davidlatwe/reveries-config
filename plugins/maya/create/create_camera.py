9b4afd45e29cbeaaa34478f945af04082169d55f
davidlatwe <davidlatwe@gmail.com>
2020-03-11 12:26:12 +0800

    Bug fix for 7af209
    After 7af209, submitting Deadline will require each instance has it's own Deadline Job data like pool and priority. The instance of camera does not have those so the publish will error out when submitting to Deadline.

--- plugins/maya/create/create_camera.py
+++ plugins/maya/create/create_camera.py
@@ -1,6 +1,7 @@
 
 import avalon.maya
 from reveries.maya.pipeline import put_instance_icon
+from reveries import lib
 
 
 class CameraCreator(avalon.maya.Creator):
@@ -14,5 +15,8 @@ class CameraCreator(avalon.maya.Creator):
         self.data["bakeStep"] = 1.0
         # Apply Euler filter to rotations for Alembic
         self.data["eulerFilter"] = False
+
+        self.data["deadlinePriority"] = 80
+        self.data["deadlinePool"] = lib.get_deadline_pools()
 
         return put_instance_icon(super(CameraCreator, self).process())
