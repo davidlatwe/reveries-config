937eeb704894b6fe76033b1dbe6af3b83972f896
davidlatwe <davidlatwe@gmail.com>
2020-03-19 23:06:42 +0800

    Merge Maya Deadline submitting plugins

--- plugins/maya/publish/submit_deadline_publish.py
+++ plugins/maya/publish/submit_deadline_publish.py
@@ -1,5 +1,6 @@
 
 import os
+import copy
 import json
 import platform
 import pyblish.api
@@ -12,23 +13,24 @@ class SubmitDeadlinePublish(pyblish.api.ContextPlugin):
 
     """
 
-    order = pyblish.api.ExtractorOrder + 0.3
+    order = pyblish.api.ExtractorOrder + 0.492
     hosts = ["maya"]
     label = "Deadline Publish"
 
     targets = ["deadline"]
 
+    RENDER_TYPES = [
+        "reveries.renderlayer",
+    ]
+
     def process(self, context):
         import reveries
-
-        reveries_path = reveries.__file__
-        script_file = os.path.join(os.path.dirname(reveries_path),
-                                   "scripts",
-                                   "deadline_publish.py")
 
         if not all(result["success"] for result in context.data["results"]):
             self.log.warning("Atomicity not held, aborting.")
             return
+
+        reveries_path = reveries.__file__
 
         # Context data
 
@@ -60,9 +62,6 @@ class SubmitDeadlinePublish(pyblish.api.ContextPlugin):
             if instance.data.get("isDependency", False):
                 continue
 
-            if instance.data.get("submitted", False):
-                continue
-
             subset = instance.data["subset"]
             version = instance.data["versionNext"]
 
@@ -70,10 +69,31 @@ class SubmitDeadlinePublish(pyblish.api.ContextPlugin):
             deadline_prio = instance.data["deadlinePriority"]
             deadline_group = instance.data.get("deadlineGroup")
 
+            frame_per_task = instance.data.get("deadlineFramesPerTask", 1)
+
+            try:
+                frame_start = int(instance.data["startFrame"])
+                frame_end = int(instance.data["endFrame"])
+                frame_step = int(instance.data["byFrameStep"])
+
+            except KeyError:
+                frames = None
+            else:
+                frames = "{start}-{end}x{step}".format(
+                    start=frame_start,
+                    end=frame_end,
+                    step=frame_step,
+                )
+
             job_name = "{subset} v{version:0>3}".format(
                 subset=subset,
                 version=version,
             )
+
+            if instance.data.get("deadlineSuspendJob", False):
+                init_state = "Suspended"
+            else:
+                init_state = "Active"
 
             # Assemble payload
 
@@ -86,31 +106,88 @@ class SubmitDeadlinePublish(pyblish.api.ContextPlugin):
                     "MachineName": platform.node(),
                     "Comment": comment,
                     "Pool": deadline_pool,
-                    "Priority": deadline_prio,
                     "Group": deadline_group,
+                    "Priority": deadline_prio,
+
+                    "Frames": frames,
+                    "ChunkSize": frame_per_task,
+                    "InitialStatus": init_state,
 
                     "ExtraInfo0": project["name"],
                 },
-                "PluginInfo": {
 
-                    "ScriptJob": True,
-                    "ScriptFilename": script_file,
+                # Mandatory for Deadline, may be empty
+                "AuxFiles": [],
+                "IdOnly": True
+            }
 
+            if instance.data.get("hasYeti"):
+                # Change Deadline group for Yeti
+                payload["JobInfo"]["Group"] = "yeti_render"
+
+            if instance.data["family"] in self.RENDER_TYPES:
+                # Render Job
+                output_dir = context.data["outputDir"].replace("\\", "/")
+                has_renderlayer = context.data["hasRenderLayers"]
+                use_rendersetup = context.data["usingRenderSetup"]
+
+                output_prefix = instance.data["fileNamePrefix"]
+                renderlayer = instance.data["renderlayer"]
+                renderer = instance.data["renderer"]
+                rendercam = instance.data["camera"]
+
+                payload["PluginInfo"] = {
                     # Input
                     "SceneFile": fpath,
                     # Resolve relative references
                     "ProjectPath": workspace,
                     # Mandatory for Deadline
                     "Version": maya_version,
+                    # Output directory and filename
+                    "OutputFilePath": output_dir,
+                    "OutputFilePrefix": output_prefix,
+
+                    "UsingRenderLayers": has_renderlayer,
+                    "UseLegacyRenderLayers": not use_rendersetup,
+                    "RenderLayer": renderlayer,
+                    "Renderer": renderer,
+                    "Camera": rendercam,
                 },
-                # Mandatory for Deadline, may be empty
-                "AuxFiles": [],
-                "IdOnly": True
-            }
+            else:
+                # Script Job
+                script_file = os.path.join(os.path.dirname(reveries_path),
+                                           "scripts",
+                                           "deadline_extract.py")
+
+                payload["PluginInfo"] = {
+                    "ScriptJob": True,
+                    "ScriptFilename": script_file,
+                    # Input
+                    "SceneFile": fpath,
+                    # Resolve relative references
+                    "ProjectPath": workspace,
+                    # Mandatory for Deadline
+                    "Version": maya_version,
+                }
 
             # Environment
 
             environment = self.assemble_environment(instance)
+
+            if instance.data.get("hasAtomsCrowds"):
+                # Change Deadline group for AtomsCrowd
+                payload["JobInfo"]["Group"] = "atomscrowd"
+            else:
+                # AtomsCrowd module path is available for every machine by
+                # default, so we must remove it if this renderLayer does
+                # not require AtomsCrowd plugin. Or the license will not
+                # be enough for other job that require Atoms.
+                module_path = environment["MAYA_MODULE_PATH"]
+                filtered = list()
+                for path in module_path.split(";"):
+                    if "AtomsMaya" not in path:
+                        filtered.append(path)
+                environment["MAYA_MODULE_PATH"] = ";".join(filtered)
 
             parsed_environment = {
                 "EnvironmentKeyValue%d" % index: u"{key}={value}".format(
@@ -128,6 +205,29 @@ class SubmitDeadlinePublish(pyblish.api.ContextPlugin):
             # Submit
 
             submitter = context.data["deadlineSubmitter"]
+            index = submitter.add_job(payload)
+
+            # Filesys publish script
+
+            payload = copy.deepcopy(payload)
+
+            script_file = os.path.join(os.path.dirname(reveries_path),
+                                       "scripts",
+                                       "filesys_publish.py")
+            # Clean up
+            payload["JobInfo"].pop("Frames")
+            payload["JobInfo"].pop("ChunkSize")
+            # Update
+            payload["JobInfo"].update({
+                "Name": "|| Publish: " + payload["JobInfo"]["Name"],
+                "JobDependencies": index,
+                "InitialStatus": "Active",
+            })
+            payload["PluginInfo"].update({
+                "ScriptJob": True,
+                "ScriptFilename": script_file,
+            })
+
             submitter.add_job(payload)
 
     def assemble_environment(self, instance):
@@ -140,7 +240,7 @@ class SubmitDeadlinePublish(pyblish.api.ContextPlugin):
 
         """
         submitter = instance.context.data["deadlineSubmitter"]
-        environment = submitter.instance_env(instance)
+        environment = submitter.environment()
 
         # From current environment
         for var in [
