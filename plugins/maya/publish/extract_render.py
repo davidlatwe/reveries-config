1a3bcaa21f7d3fa60096a2d9aab85cf8c40ee925
davidlatwe <davidlatwe@gmail.com>
2020-03-27 22:01:12 +0800

    Refactor render extractor

--- plugins/maya/publish/extract_render.py
+++ plugins/maya/publish/extract_render.py
@@ -1,12 +1,11 @@
 
 import os
 import pyblish.api
-import reveries.utils
-from reveries import plugins
-from reveries.maya import utils
+from reveries import utils
+from reveries.maya import utils as maya_utils
 
 
-class ExtractRender(plugins.PackageExtractor):
+class ExtractRender(pyblish.api.InstancePlugin):
     """Start GUI rendering if not delegate to Deadline
     """
 
@@ -18,70 +17,70 @@ class ExtractRender(plugins.PackageExtractor):
         "reveries.renderlayer",
     ]
 
-    representations = [
-        "renderLayer",
-    ]
-
-    def extract_renderLayer(self, instance):
+    def process(self, instance):
         """Extract per renderlayer that has AOVs (Arbitrary Output Variable)
-        """
-        packager = instance.data["packager"]
-        packager.skip_stage()
-        package_path = packager.create_package()
-
-        self.log.info("Computing render output path..")
-
-        # Computing output path may take a while
-        output_dir = instance.context.data["outputDir"]
-        output_paths = utils.get_output_paths(output_dir,
-                                              instance.data["renderer"],
-                                              instance.data["renderlayer"],
-                                              instance.data["camera"])
-        instance.data["outputPaths"] = output_paths
-
-        # Assume the rendering has been completed at this time being,
-        # start to check and extract the rendering outputs
-        for aov_name, aov_path in output_paths.items():
-            self.compute_filenames(instance, aov_path, aov_name, package_path)
-
-        self.render()
-
-    def compute_filenames(self, instance, aov_path, aov_name, package_path):
-        """
         """
         from maya import cmds
 
-        seq_dir, pattern = os.path.split(aov_path)
+        self.log.info("Computing render output path..")
 
-        # (NOTE) Did not consider frame step (byFrame)
-        start_frame = instance.data["startFrame"]
-        end_frame = instance.data["endFrame"]
-        by_frame = instance.data["byFrameStep"]
-
-        project = instance.context.data["projectDoc"]
-        e_in, e_out, handles, _ = reveries.utils.get_timeline_data(project)
+        renderer = instance.data["renderer"]
+        renderlayer = instance.data["renderlayer"]
         camera = instance.data["camera"]
 
-        packager = instance.data["packager"]
-        packager.add_data({"sequence": {
-            aov_name: {
+        # Computing output path may take a while
+        staging_dir = instance.context.data["outputDir"]
+        outputs = maya_utils.get_output_paths(staging_dir,
+                                              renderer,
+                                              renderlayer,
+                                              camera)
+        padding = maya_utils.get_render_padding(renderer)
+        padding_str = "#" * padding
+        frame_str = "%%0%dd" % padding
+
+        # Assume the rendering has been completed at this time being,
+        # start to check and extract the rendering outputs
+        sequence = dict()
+        files = list()
+        for aov_name, aov_path in outputs.items():
+
+            pattern = os.path.relpath(aov_path, staging_dir)
+
+            start = instance.data["startFrame"]
+            end = instance.data["endFrame"]
+            step = instance.data["byFrameStep"]
+
+            project = instance.context.data["projectDoc"]
+            e_in, e_out, handles, _ = utils.get_timeline_data(project)
+
+            sequence[aov_name] = {
                 "imageFormat": instance.data["fileExt"],
-                "fname": pattern,
-                "seqSrcDir": seq_dir,
-                "startFrame": start_frame,
-                "endFrame": end_frame,
-                "byFrameStep": by_frame,
+                "fpattern": pattern,
+                "startFrame": start,
+                "endFrame": end,
+                "byFrameStep": step,
                 "edit_in": e_in,
                 "edit_out": e_out,
                 "handles": handles,
                 "focalLength": cmds.getAttr(camera + ".focalLength"),
                 "resolution": instance.data["resolution"],
                 "fps": instance.context.data["fps"],
-                "cameraUUID": utils.get_id(camera),
-                "renderlayer": instance.data["renderlayer"],
+                "cameraUUID": maya_utils.get_id(camera),
+                "renderlayer": renderlayer,
             }
-        }})
 
-    @plugins.delay_extract
+            fname = pattern.replace(padding_str, frame_str)
+            for frame_num in range(start, end, step):
+                files.apppend(fname % frame_num)
+
+        instance.data["outputPaths"] = outputs
+
+        instance.data["repr.renderLayer._stage"] = staging_dir
+        instance.data["repr.renderLayer._hardlinks"] = files
+        instance.data["repr.renderLayer.sequence"] = sequence
+        instance.data["repr.renderLayer._delayRun"] = {
+            "func": self.render,
+        }
+
     def render(self):
         pass
