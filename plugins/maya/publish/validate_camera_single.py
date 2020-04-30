fb0ac1e7a321377519599a10cee01792b791a033
davidlatwe <davidlatwe@gmail.com>
2020-04-30 16:17:47 +0800

    Add support to stereo cam validation

--- plugins/maya/publish/validate_camera_single.py
+++ plugins/maya/publish/validate_camera_single.py
@@ -16,10 +16,14 @@ class ValidateSingleCamera(pyblish.api.InstancePlugin):
     def process(self, instance):
         from maya import cmds
 
-        cameras = cmds.ls(instance[:], type="camera")
+        stereo_cameras = cmds.ls(instance, type="stereoRigCamera")
+        cameras = cmds.ls(instance, exactType="camera")
 
-        if len(cameras) == 0:
+        if len(cameras) == 0 and len(stereo_cameras) == 0:
             raise Exception("No camera collected.")
 
-        if len(cameras) > 1:
-            raise Exception("Multiple cameras" "found: %s " % cameras)
+        if (len(stereo_cameras) > 1
+                or (len(stereo_cameras) == 1 and len(cameras) > 2)
+                or (len(stereo_cameras) == 0 and len(cameras) > 1)):
+            raise Exception("Multiple cameras found: %s "
+                            % (stereo_cameras + cameras))
