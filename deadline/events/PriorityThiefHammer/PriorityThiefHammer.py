a3b1cd13e11a231d7e36093b3b601172d1cbd636
davidlatwe <davidlatwe@gmail.com>
2020-05-19 16:23:10 +0800

    Add Deadline event plugin "PriorityThiefHammer"

---
+++ deadline/events/PriorityThiefHammer/PriorityThiefHammer.py
@@ -0,0 +1,26 @@
+
+import Deadline.Events
+from Deadline.Scripting import RepositoryUtils
+
+
+def GetDeadlineEventListener():
+    return PriorityThiefHammerOnJobSubmitted()
+
+
+def CleanupDeadlineEventListener(eventListener):
+    eventListener.Cleanup()
+
+
+class PriorityThiefHammerOnJobSubmitted(Deadline.Events.DeadlineEventListener):
+
+    def __init__(self):
+        self.OnJobSubmittedCallback += self.OnJobSubmitted
+
+    def Cleanup(self):
+        del self.OnJobSubmittedCallback
+
+    def OnJobSubmitted(self, job):
+        if job.JobPriority >= 80:
+            job.JobPriority = 70
+            RepositoryUtils.SaveJob(job)
+            print("Job submitted with priority over 80. Fall back to 70, Bam !")
---
+++ deadline/events/PriorityThiefHammer/README.md
@@ -0,0 +1,4 @@
+Deadline Job Priority Inspector Event Plugin
+===
+
+An event plugin that reacts on job submission to check priority and pound to 70 if the value is over 80.
