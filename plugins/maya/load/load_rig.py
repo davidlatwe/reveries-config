b18e85aaeb8a2812fe526f008ef6ca5ad9378067
davidlatwe <davidlatwe@gmail.com>
2020-05-26 18:01:17 +0800

    Avoid garbage collect from cache in setdress load

--- plugins/maya/load/load_rig.py
+++ plugins/maya/load/load_rig.py
@@ -1,12 +1,7 @@
 
 import contextlib
 import avalon.api
-import avalon.maya
-
 from avalon.vendor import qargparse
-
-from reveries import utils
-from reveries.maya import pipeline
 from reveries.maya.plugins import ReferenceLoader
 
 
@@ -57,13 +52,16 @@ class RigLoader(ReferenceLoader, avalon.api.Loader):
 
     def update(self, container, representation):
         from maya import cmds
+        from reveries import utils
+        from reveries.maya import pipeline
+        import avalon.io
 
         node = container["objectName"]
 
         # Get reference node from container
         reference_node = self.get_reference_node(container)
 
-        with patch(reference_node):
+        with self.patch(reference_node):
 
             parents = avalon.io.parenthood(representation)
             self.package_path = utils.get_representation_path_(representation,
@@ -99,92 +97,91 @@ class RigLoader(ReferenceLoader, avalon.api.Loader):
                                       version,
                                       representation)
 
+    @contextlib.contextmanager
+    def patch(self, reference_node):
+        """A patch after commit 06c2ef9 and should be removed ASAP
 
-@contextlib.contextmanager
-def patch(reference_node):
-    """A patch after commit 06c2ef9 and should be removed ASAP
+        Before commit 06c2ef9, published rig has no namespace on imported
+        models or shaders.
 
-    Before commit 06c2ef9, published rig has no namespace on imported
-    models or shaders.
+        And sometimes animator may make their own change on referenced rig
+        that may create extra meshes under referenced transform node, which
+        Maya will not able to re-apply those edits when replacing reference
+        if the original rig was published before commit 06c2ef9 and the new
+        rig was published after that (because new node will have namespace).
 
-    And sometimes animator may make their own change on referenced rig
-    that may create extra meshes under referenced transform node, which
-    Maya will not able to re-apply those edits when replacing reference
-    if the original rig was published before commit 06c2ef9 and the new
-    rig was published after that (because new node will have namespace).
+        Hence we need this patch for the transition, and will remove this once
+        we could pin config version on each project.
 
-    Hence we need this patch for the transition, and will remove this once
-    we could pin config version on each project.
+        """
+        from maya import cmds
 
-    """
-    from maya import cmds
+        referenced = cmds.referenceQuery(reference_node,
+                                        nodes=True,
+                                        dagPath=True)
+        transforms = cmds.ls(referenced, type="transform", long=True)
+        meshes = cmds.listRelatives(transforms,
+                                    shapes=True,
+                                    fullPath=True,
+                                    type="mesh") or []
 
-    referenced = cmds.referenceQuery(reference_node,
-                                     nodes=True,
-                                     dagPath=True)
-    transforms = cmds.ls(referenced, type="transform", long=True)
-    meshes = cmds.listRelatives(transforms,
-                                shapes=True,
-                                fullPath=True,
-                                type="mesh") or []
+        # Collect meshes(uuid) that were created in scene
 
-    # Collect meshes(uuid) that were created in scene
-
-    mesh_uuids = dict()
-    for mesh in meshes:
-        if cmds.referenceQuery(mesh, isNodeReferenced=True):
-            continue
-
-        parent = cmds.ls(mesh.rsplit("|", 1)[0], uuid=True)[0]
-        shading = cmds.ls(cmds.listConnections(mesh,
-                                               source=False,
-                                               destination=True,
-                                               connections=True,
-                                               type="shadingEngine"),
-                          uuid=True)
-        mesh = cmds.ls(mesh, uuid=True)[0]
-        mesh_uuids[mesh] = (parent, shading)
-
-    yield
-
-    # Adopt new parent
-
-    foster = cmds.listConnections(reference_node + ".fosterParent")
-    if foster and mesh_uuids:
-        foster = foster[0]
-
-        shapes = cmds.listRelatives(foster, shapes=True, fullPath=True)
-        shapes = cmds.ls(shapes, uuid=True)
-        for uuid in shapes:
-            if uuid not in mesh_uuids:
+        mesh_uuids = dict()
+        for mesh in meshes:
+            if cmds.referenceQuery(mesh, isNodeReferenced=True):
                 continue
 
-            parent, shading = mesh_uuids[uuid]
-            parent = cmds.ls(parent)
-            fostered_shape = cmds.ls(uuid, long=True)
+            parent = cmds.ls(mesh.rsplit("|", 1)[0], uuid=True)[0]
+            shading = cmds.ls(cmds.listConnections(mesh,
+                                                source=False,
+                                                destination=True,
+                                                connections=True,
+                                                type="shadingEngine"),
+                            uuid=True)
+            mesh = cmds.ls(mesh, uuid=True)[0]
+            mesh_uuids[mesh] = (parent, shading)
 
-            if parent and fostered_shape:
-                # Move fostered mesh node to newly referenced transform node
-                fostered_shape = fostered_shape[0]
-                shape = cmds.parent(fostered_shape,
-                                    parent[0],
-                                    addObject=True,
-                                    shape=True)
-                cmds.parent(fostered_shape,
-                            removeObject=True,
-                            shape=True)
+        yield
 
-                # Try to fix shading
-                if shading:
-                    shading = iter(shading)
-                    for src, dst in zip(shading, shading):
-                        dst = cmds.ls(dst)
-                        if not dst:
-                            continue
-                        src = shape[0] + "." + src.split(".", 1)[-1]
-                        dst = dst[0] + ".dagSetMembers"
+        # Adopt new parent
 
-                        try:
-                            cmds.connectAttr(src, dst, nextAvailable=True)
-                        except Exception as e:
-                            cmds.warning(str(e))
+        foster = cmds.listConnections(reference_node + ".fosterParent")
+        if foster and mesh_uuids:
+            foster = foster[0]
+
+            shapes = cmds.listRelatives(foster, shapes=True, fullPath=True)
+            shapes = cmds.ls(shapes, uuid=True)
+            for uuid in shapes:
+                if uuid not in mesh_uuids:
+                    continue
+
+                parent, shading = mesh_uuids[uuid]
+                parent = cmds.ls(parent)
+                fostered_shape = cmds.ls(uuid, long=True)
+
+                if parent and fostered_shape:
+                    # Move fostered mesh node to newly referenced transform node
+                    fostered_shape = fostered_shape[0]
+                    shape = cmds.parent(fostered_shape,
+                                        parent[0],
+                                        addObject=True,
+                                        shape=True)
+                    cmds.parent(fostered_shape,
+                                removeObject=True,
+                                shape=True)
+
+                    # Try to fix shading
+                    if shading:
+                        shading = iter(shading)
+                        for src, dst in zip(shading, shading):
+                            dst = cmds.ls(dst)
+                            if not dst:
+                                continue
+                            src = shape[0] + "." + src.split(".", 1)[-1]
+                            dst = dst[0] + ".dagSetMembers"
+
+                            try:
+                                cmds.connectAttr(src, dst, nextAvailable=True)
+                            except Exception as e:
+                                cmds.warning(str(e))
