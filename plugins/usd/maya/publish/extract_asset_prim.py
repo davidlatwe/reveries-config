334574c5c7fccd5ee01f03623ced7678a6ee4dd0
rebeccalin209 <rebeccalin209@gmail.com>
2020-09-04 18:41:08 +0800

    render/proxy option in houdini

--- plugins/usd/maya/publish/extract_asset_prim.py
+++ plugins/usd/maya/publish/extract_asset_prim.py
@@ -24,7 +24,7 @@ class ExtractAssetPrimUSDExport(pyblish.api.InstancePlugin):
         from pxr import Usd, UsdGeom
 
         from reveries import utils
-        from reveries.usd.utils import load_maya_plugin
+        from reveries.usd.utils import load_maya_plugin, asset_prim_export
         from reveries.new_utils import get_publish_files
 
         asset_doc = instance.data["assetDoc"]
@@ -40,40 +40,44 @@ class ExtractAssetPrimUSDExport(pyblish.api.InstancePlugin):
         instance.data["repr.USD.entryFileName"] = file_name
 
         # Get asset id
-        _filter = {"type": "asset", "name": asset_name}
-        asset_id = io.find_one(_filter)['_id']
+        # _filter = {"type": "asset", "name": asset_name}
+        # asset_id = io.find_one(_filter)['_id']
 
         load_maya_plugin()
 
-        subsets = ['renderPrim', 'proxyPrim']
-        usd_files = {}
-
-        for subset_name in subsets:
-            _filter = {
-                "type": "subset",
-                "name": subset_name,
-                "parent": asset_id
-            }
-            # print('subset_name: ', subset_name)
-            subset_data = io.find_one(_filter)
-            if subset_data:
-                subset_id = subset_data['_id']
-                usd_files[subset_name] = get_publish_files.get_files(subset_id).get('USD', [])
-
-        stage = Usd.Stage.CreateInMemory()
-
-        UsdGeom.Xform.Define(stage, "/ROOT")
-        rootPrim = stage.GetPrimAtPath('/ROOT')
-        stage.SetDefaultPrim(rootPrim)
-
-        root_layer = stage.GetRootLayer()
-
-        for _, paths in usd_files.items():
-            if paths:
-                root_layer.subLayerPaths.append(paths[0])
-
-        # print(stage.GetRootLayer().ExportToString())
+        # Export asset prim usd
         output_path = os.path.join(staging_dir, file_name)
-        stage.GetRootLayer().Export(output_path)
+        asset_prim_export.export(asset_name, output_path)
+
+        # subsets = ['renderPrim', 'proxyPrim']
+        # usd_files = {}
+        #
+        # for subset_name in subsets:
+        #     _filter = {
+        #         "type": "subset",
+        #         "name": subset_name,
+        #         "parent": asset_id
+        #     }
+        #     # print('subset_name: ', subset_name)
+        #     subset_data = io.find_one(_filter)
+        #     if subset_data:
+        #         subset_id = subset_data['_id']
+        #         usd_files[subset_name] = get_publish_files.get_files(subset_id).get('USD', [])
+        #
+        # stage = Usd.Stage.CreateInMemory()
+        #
+        # UsdGeom.Xform.Define(stage, "/ROOT")
+        # rootPrim = stage.GetPrimAtPath('/ROOT')
+        # stage.SetDefaultPrim(rootPrim)
+        #
+        # root_layer = stage.GetRootLayer()
+        #
+        # for _, paths in usd_files.items():
+        #     if paths:
+        #         root_layer.subLayerPaths.append(paths[0])
+        #
+        # # print(stage.GetRootLayer().ExportToString())
+        # output_path = os.path.join(staging_dir, file_name)
+        # stage.GetRootLayer().Export(output_path)
 
         print 'Export asset prim usd done.'
