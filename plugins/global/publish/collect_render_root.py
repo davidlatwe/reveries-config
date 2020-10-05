679a48146e5458f76b3c36d533dbfa0e20e93fc0
davidlatwe <davidlatwe@gmail.com>
2020-10-05 19:09:53 +0800

    Use pre-defined render root if given

--- plugins/global/publish/collect_render_root.py
+++ plugins/global/publish/collect_render_root.py
@@ -12,9 +12,15 @@ class CollectRenderRoot(pyblish.api.InstancePlugin):
     ]
 
     def process(self, instance):
+        render_root = instance.data.get("reprRoot")
+        if render_root:
+            self.log.info("Pre-defined render output root: %s" % render_root)
+            return
+
         project = instance.context.data["projectDoc"]
         project_render_root = project["data"].get("renderRoot")
         instance_render_root = instance.data.get("renderRoot", "").strip()
+
         render_root = instance_render_root or project_render_root
         if render_root:
             instance.data["reprRoot"] = render_root
